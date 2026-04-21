1f818ff6ddf3ad38e4ee1cfa12c1b11677a466ce
justinpaek22 <161536767+justinpaek22@users.noreply.github.com>
2026-04-21 14:36:46 -0400

    Delete delayspikes_bymonth.sql

--- delayspikes_bymonth.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    MONTH(flight_date) AS month,
-    ROUND(AVG(departure_delay), 2) AS avg_departure_delay,
-    ROUND(AVG(arrival_delay), 2) AS avg_arrival_delay
-FROM flight_combined
-WHERE airport = 'ATL'
-GROUP BY MONTH(flight_date)
